2379f78cc30b12546f82f22c306edeeceec9bf66
Kishore <Kishore@192.168.1.37>
2025-05-15 16:47:55 +0530

    program to say a hello

---
+++ src/test/java/sample/say_hello.java
@@ -0,0 +1,11 @@
+package sample;
+
+public class say_hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HELLO..............");
+	}
+
+}
