7ab36460561c7536f22b67c3b1eda137b4d3b89e
Erohsik275 <kishorerajendran275@gmail.com>
2025-05-16 16:53:45 +0530

    initial project -2

---
+++ src/test/java/basic_project/print_statement.java
@@ -0,0 +1,11 @@
+package basic_project;
+
+public class print_statement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("HI............");
+	}
+
+}
