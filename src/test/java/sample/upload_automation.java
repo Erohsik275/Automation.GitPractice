136d1a70e89d2372b1837c9cd7ce9fea9e270ebc
Erohsik275 <kishorerajendran275@gmail.com>
2025-05-17 15:55:40 +0530

    tc01

--- src/test/java/sample/upload_automation.java
+++ src/test/java/sample/upload_automation.java
@@ -1,14 +1,20 @@
 package sample;
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 public class upload_automation {
-	public static void main(String[] args) {
+	public static void main(String[] args) throws Throwable {
 		ChromeDriver driver = new ChromeDriver();
 		driver.get("https://the-internet.herokuapp.com/upload");
 		driver.manage().window().maximize();
-		driver.findElement(By.id("file-upload"));
+		WebDriverWait wait = new WebDriverWait(driver, Duration.ofSeconds(10));
+		wait.until(ExpectedConditions.visibilityOfAllElementsLocatedBy(By.id("file-upload")));
+		driver.findElement(By.id("file-upload")).click();
 		
 	}
 
